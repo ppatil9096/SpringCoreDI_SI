4c14dc3e288fd72ffaf39be4859e0f1d558ad194
ppatil <ppatil@localhost>
2018-05-28 17:44:08 +0530

    committed the autowiring bean definations for byType, byTname, constructor, No. and its Test class

--- SpringCoreDI_SI/src/com/pravin/spring/tutorial/test/TestAutowiring.java
+++ SpringCoreDI_SI/src/com/pravin/spring/tutorial/test/TestAutowiring.java
@@ -9,7 +9,17 @@ public class TestAutowiring {
     public static void main(String[] args) {
 	@SuppressWarnings("resource")
 	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-	A_Autowiring a_Autowiring = applicationContext.getBean("aAutowiring", A_Autowiring.class);
-	a_Autowiring.display();
+	// byName
+	/*A_Autowiring autowiringName = applicationContext.getBean("aAutowiringName", A_Autowiring.class);
+	autowiringName.display();
+	// byType
+	A_Autowiring autowiringType = applicationContext.getBean("aAutowiringType", A_Autowiring.class);
+	autowiringType.display();
+	A_Autowiring autowiringConstructor = applicationContext.getBean("aAutowiringConstructor", A_Autowiring.class);
+	autowiringConstructor.display();
+	*/
+	A_Autowiring autowiringNo = applicationContext.getBean("aAutowiringNo", A_Autowiring.class);
+	autowiringNo.display();
+	
     }
 }
